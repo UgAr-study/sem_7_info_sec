e86de8bc137bcc8bcd18d9687802ee47c15a62a7
Dmitrryy <da.drozdov@mail.ru>
2022-11-22 19:41:01 +0300

    [Goppa] init - simple GF arithmetic

---
+++ goppa/include/GF.h
@@ -0,0 +1,16 @@
+#include <string>
+
+//This returns a*b in GF(2^8)
+unsigned int galois_mul(unsigned int a, unsigned int b, unsigned int P_x);
+
+//This gives you the inverse in GF(2^8);
+unsigned int inverse(unsigned int input, unsigned int P_x);
+
+//this returns input mod P(x)
+unsigned int modp(unsigned int input, unsigned int P_x);
+
+//this returns input^power mod P(x)
+unsigned int galois_pow(unsigned input, int power, unsigned int P_x);
+
+//this returns log(n)
+int highest_degree(unsigned int a);
