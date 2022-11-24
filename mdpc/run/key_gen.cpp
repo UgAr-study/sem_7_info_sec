c0ee5fa09a5d09022e9d1423f54614d0a40ed90c
UgAr-study <uglovskij.au@gmail.com>
2022-11-24 23:54:04 +0300

    cli parser in key_gen fixed

--- mdpc/run/key_gen.cpp
+++ mdpc/run/key_gen.cpp
@@ -7,20 +7,19 @@
 
 int main(int argc, char const *argv[]) 
 {
-    int n0, p, w, t;
+    int n0 = 0, p = 0, w = 0;
     CLI::App app{"Key generation"};
 
     app.add_option("--n0", n0, "Number of circular blocks in H matrix");
     app.add_option("--p", p, "Permutation size of circular block");
     app.add_option("--w", w, "Row weight");
-    app.add_option("--t", t, "Error vector weight");
 
     CLI11_PARSE(app, argc, argv);
 
     const std::string fName_private = "private.txt";
     const std::string fName_public = "public.txt";
 
-    qc_mdpc key(n0, p, w, t);
+    qc_mdpc key(n0, p, w);
     auto&& HMatrix = key.parity_check_matrix();
     auto&& GMatrix = key.generator_matrix();
 
